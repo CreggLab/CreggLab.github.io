df8dd4897e7524ea0e01d4834fa7a9c4953ca6ed
Cregg Lab <157396599+CreggLab@users.noreply.github.com>
2024-02-26 15:24:49 +0100

    Update slideshow3.js

--- _scripts/slideshow3.js
+++ _scripts/slideshow3.js
@@ -2,18 +2,19 @@ let slideIndex3 = 0;
 let slides3 = document.getElementsByClassName("mySlides3");
 
 function showSlides3() {
-  // Hide all slides
   for (let i = 0; i < slides3.length; i++) {
-    slides3[i].style.display = "none";
+    slides3[i].style.display = "none";  
   }
-
-  // Show the next slide
   slideIndex3++;
-  if (slideIndex3 > slides3.length) {slideIndex3 = 1} // Loop back to the first slide
-  slides3[slideIndex3 - 1].style.display = "block";
+  if (slideIndex3 > slides3.length) {slideIndex3 = 1}    
+  slides3[slideIndex3-1].style.display = "block";  
+  // Reset timer with each call to showSlides3
+  clearTimeout(timer);
+  timer = setTimeout(showSlides3, 6000); // Change slide every 6 seconds
+}
 
-  // Set the timer for the next slide
-  setTimeout(showSlides3, 8000); // Change slide every 6 seconds
+function plusSlides3(n) {
+  showSlides3(slideIndex3 += n - 1);
 }
 
 // Start the slideshow
