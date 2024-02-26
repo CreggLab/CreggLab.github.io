e1b5a3c325a45152da66921350c2bf589dafe3b9
CreggLab <157396599+CreggLab@users.noreply.github.com>
2024-02-26 14:41:09 +0000

    Deploying to gh-pages from @ CreggLab/CreggLab.github.io@cf0ddc7611892a838702ac30c65c2d7d9d258261 🚀

--- _scripts/slideshow3.js
+++ _scripts/slideshow3.js
@@ -2,22 +2,35 @@ let slideIndex3 = 1; // Start from the first slide
 let slides3 = document.getElementsByClassName("mySlides3");
 let timer;
 
-// Initialize the slideshow
+// Initialize the slideshow with the first slide
 showSlides3(slideIndex3);
 
+// Function to move to the next or previous slide
 function plusSlides3(n) {
-  clearTimeout(timer); // Clear the existing timer
-  showSlides3(slideIndex3 += n);
+  clearTimeout(timer); // Clear the existing timer to reset the slideshow timing
+  showSlides3(slideIndex3 += n); // Adjust the slide index and display the new slide
 }
 
+// Function to display the slide based on the current slide index
 function showSlides3(n) {
   let i;
-  if (n > slides3.length) { slideIndex3 = 1 }    
-  if (n < 1) { slideIndex3 = slides3.length }
-  for (i = 0; i < slides3.length; i++) {
-     slides3[i].style.display = "none";  
+  if (n > slides3.length) { 
+    slideIndex3 = 1; // If moving beyond the last slide, wrap to the first slide
+  } else if (n < 1) { 
+    slideIndex3 = slides3.length; // If moving before the first slide, wrap to the last slide
   }
-  slides3[slideIndex3-1].style.display = "block";  
-  timer = setTimeout(function() { plusSlides3(1); }, 6000); // Change slide every 6 seconds
+
+  // Hide all slides
+  for (i = 0; i < slides3.length; i++) {
+    slides3[i].style.display = "none";  
+  }
+
+  // Display the current slide
+  slides3[slideIndex3 - 1].style.display = "block";  
+
+  // Set the timer to automatically move to the next slide after 6 seconds
+  timer = setTimeout(function() { plusSlides3(1); }, 6000);
 }
 
+// Start the slideshow
+showSlides3(slideIndex3);
