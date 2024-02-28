8ef7d1064734f2c7719aab4c8441f0bf76589291
Cregg Lab <157396599+CreggLab@users.noreply.github.com>
2024-02-28 15:20:21 +0100

    Update enlarge.js

--- _scripts/enlarge.js
+++ _scripts/enlarge.js
@@ -1,7 +1,4 @@
 function enlargeImg() {
-  // Only allow enlargement if screen width is greater than 800px
-  if (window.innerWidth > 800) {
     var element = document.getElementById("myTable");
     element.classList.toggle("largetable");
   }
-}
